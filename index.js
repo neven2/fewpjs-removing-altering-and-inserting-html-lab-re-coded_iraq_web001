363b5caaf9793472a3fefdea91e1824ec13a7591
Neven Bedr <neven2225@gmail.com>
2020-05-14 20:45:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = " your name is the champion";
+ 
