7fea6a815bfb77022151d896a022b7cda17911ab
Neven Bedr <neven2225@gmail.com>
2020-05-14 20:44:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+let newheader = document.createElement("h1");
+newheader.id="victory";
+
+newheader.innerHTML("YOUR-NAME is the champion");
+ 
